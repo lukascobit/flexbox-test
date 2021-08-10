57b29321562885e065d5324e2e18983a48610da0
Lukas <lukyn.odehnal333@gmail.com>
2021-08-10 20:04:36 +0200

    test0

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,46 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>hello</h1>
+      <h1>test</h1>
+      <div className="flexParent">
+        <div className="flex a">
+          <h1>A</h1>
+          <p>This is the body. Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet illum libero quisquam maxime quo consequuntur explicabo nulla veniam distinctio, totam nostrum quasi quidem. Doloribus dolorum, sunt dolorem commodi quo recusandae.</p>
+        </div>
+        <div className="flex b">
+          <h1>B</h1>
+          <p>This is the body. Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet illum libero quisquam maxime quo consequuntur explicabo nulla veniam distinctio, totam nostrum quasi quidem. Doloribus dolorum, sunt dolorem commodi quo recusandae.</p>
+        </div>
+        <div className="flex c">
+          <h1>C</h1>
+          <p>This is the body. Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet illum libero quisquam maxime quo consequuntur explicabo nulla veniam distinctio, totam nostrum quasi quidem. Doloribus dolorum, sunt dolorem commodi quo recusandae.</p>
+        </div>
+        <div className="flex d ">
+          <h1>D</h1>
+          <p>This is the body. Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet illum libero quisquam maxime quo consequuntur explicabo nulla veniam distinctio, totam nostrum quasi quidem. Doloribus dolorum, sunt dolorem commodi quo recusandae.</p>
+        </div>
+        <div className="flex e">
+          <h1>E</h1>
+          <p>This is the body. Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet illum libero quisquam maxime quo consequuntur explicabo nulla veniam distinctio, totam nostrum quasi quidem. Doloribus dolorum, sunt dolorem commodi quo recusandae.</p>
+        </div>
+        <div className="flex e">
+          <h1>E</h1>
+          <p>This is the body. Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet illum libero quisquam maxime quo consequuntur explicabo nulla veniam distinctio, totam nostrum quasi quidem. Doloribus dolorum, sunt dolorem commodi quo recusandae.</p>
+        </div>
+        
+      </div>
+      <div className="flexParent second">
+        <div className="flex a">
+          <h1>A</h1>
+          <p>This is the body. Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet illum libero quisquam maxime quo consequuntur explicabo nulla veniam distinctio, totam nostrum quasi quidem. Doloribus dolorum, sunt dolorem commodi quo recusandae.</p>
+        </div>
+        <div className="flex b">
+          <h1>B</h1>
+          <p>This is the body. Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet illum libero quisquam maxime quo consequuntur explicabo nulla veniam distinctio, totam nostrum quasi quidem. Doloribus dolorum, sunt dolorem commodi quo recusandae.</p>
+        </div>
+        
+        
+      </div>
 
     </div>
   );
