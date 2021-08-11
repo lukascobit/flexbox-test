a777ba236c642d79bfceeab41fbdda0062c5fe7f
Lukas <lukyn.odehnal333@gmail.com>
2021-08-11 18:40:18 +0200

    fetching from better api and some styling

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,21 @@ import './App.css';
 function App() {
   const [data, setData] = useState()
   var [f, setF] = useState(true)
-  var srcImg = "https:picsum.photos\/200\/300"
+  const looks = ["look", "glance", "glaze", "watch", "quick look", "stare", "peek", "long look", "inpect", "view"]
+  const images = ["image", "photo", "photograph", "picture", "pic", "snap"]
+  var srcImg = "https:picsum.photos/200/300"
+  const le = looks.length
+  const phrases = [`${looks[Math.round(Math.random()*le)]} at my ${images[Math.round(Math.random()*images.length)]}`,`Take a ${looks[Math.round(Math.random()*le)]} at my ${images[Math.round(Math.random()*images.length)]} `, `this is a ${images[Math.round(Math.random()*images.length)]} i took`, `${looks[Math.round(Math.random()*le)]} at this ${images[Math.round(Math.random()*images.length)]} i found!`, `cool ${images[Math.round(Math.random()*images.length)]}`, `take a ${looks[Math.round(Math.random()*le)]}`, `${looks[Math.round(Math.random()*le)]} at this!`, "omg", `cool ${images[Math.round(Math.random()*images.length)]} i found on the internet`, "WOW"]
   
-  //https://reqres.in/api/users?page=2
 
   useEffect(()=>{
     async function getTodos(){
         try {
-            const response = await fetch("https://reqres.in/api/users?page=2");
+            const response = await fetch("https://randomuser.me/api/?results=30");
             const jsonData = await response.json();
             setData(jsonData);
             console.log(data);
+            
             setF(false)
         } catch (error) {
             console.log(error);
@@ -31,15 +35,14 @@ function App() {
       <h1>test</h1>
       <div className="flexParent">
 
-      {data && data.data.map((d)=>{
+      {data && data.results.map((d, index)=>{
         srcImg = `https:picsum.photos/20${Math.round(Math.random()*10)}/30${Math.round(Math.random()*10)}`
         return(
-          <div className="flex">
-            
-            <h1><img className="avatar" src={d.avatar} alt="avatar" />{d.first_name} {d.last_name}</h1>
+          <div key={index} className={d.gender === "male" ? "flex" : "flex F"}>
+            <h1><img className="avatar" src={d.picture.thumbnail} alt="avatar" />{d.name.first} <img className="nat" src={`https://www.countryflags.io/${d.nat}/flat/64.png`} alt={d.nat} /> {d.name.last}</h1>
             <i>{d.email}</i>
-            <p>Look at my art: </p>
-            <img src={srcImg} alt="art" />
+            <p>{phrases[Math.round(Math.random()*phrases.length)]}</p>
+            <img className="img" src={srcImg} alt="art" />
           </div>
         )
       })}
